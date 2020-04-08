561c5b4548e33144c43bc25bccc55dc0d53c7aab
Patrick Ly <patrick.ly17@gmail.com>
2020-04-07 17:40:41 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring the NY Times COVID-19 dataset. ',
+    author='Bayesian Bros',
+    license='MIT',
+)
